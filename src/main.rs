47517a20a4b23c5aa6b4311ddac1576e1e4fe317
Ax1onix <yanio.tut@gmail.com>
2025-12-23 16:36:35 +0300

    Sorting app version1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
 use std::io;
-fn read_input(n: &mut String) -> u32
+fn read_input() -> u32
 {
-    io::stdin().read_line(n).expect("Wrong input, Dummkopf!");   //Readin
-    let num: u32 = n.trim().parse().expect("NOT AN INTEGRER, SCHWEIN!");
-    num
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    let number: u32 = input.trim().parse()
+        .expect("Input not an integer");
+    number
 }
-fn main() {
-    let mut n = String::new(); 
-    println!("{}", read_input(&mut n)*2);
+fn main()
+{
+    let mut n: u32 = read_input();
+    let num: usize = n as usize;
+
+    let mut numbers:Vec<u32> = vec![0u32;num];
+
+    for i in 0..numbers.len()
+    {
+        numbers[i] = read_input();
+    }
+
+    for _i in 0..numbers.len()
+    {
+        let mut boxy = 0;
+        for x in 0..numbers.len()-1
+        {
+            if numbers[x]>numbers[x+1]
+            {
+                boxy = numbers[x];
+                numbers[x] = numbers[x+1];
+                numbers[x+1] = boxy;
+            }
+        }
+    }
+
+    for i in 0..numbers.len()
+    {
+        print!(" {}",numbers[i]);
+    }
 }
